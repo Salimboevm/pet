b9e58a43bf12edc7abd72929fd984cca42441f2b
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-11-25 16:28:08 +0000

    config env for prod

--- BlogPetProject/settings.py
+++ BlogPetProject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,9 +98,9 @@ WSGI_APPLICATION = 'BlogPetProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'post',
-        'USER': 'postgres',
-        'PASSWORD': 'psql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
         'HOST': 'localhost',
         'PORT': '5432',
     }
